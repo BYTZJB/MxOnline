4421bf2c95f0f47360f3190f1783a79e01aabe35
yangwei <zqdnr@yangweideMacBook-Pro.local>
2018-02-23 19:21:35 +0800

    修改settings的database配置

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'MxOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxonline',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
     }
 }
 
